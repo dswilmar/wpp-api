0ceb157e5498843de73bb35f97a4b8c4b89eea06
Wilmar dos Santos <dswilmar@gmail.com>
2023-07-28 22:51:59 -0300

    feat: rota para enviar whatsapp

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,32 @@
+import express, { Request, Response } from 'express';
 import Sender from "./sender";
 
 const sender = new Sender();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/status', (req: Request, res: Response) => {
+
+});
+
+app.post('/send', async(req: Request, res: Response) => {
+
+    const {message, number} = req.body;
+
+    try {
+        await sender.sendText(`${number}@c.us`, message);
+        return res.status(201).json({});
+    } catch (error) {
+        console.error("error", error);
+        res.status(500).json({
+            status: "error",
+            message: error
+        });
+    }
+});
+
+app.listen(3333, () => {
+    console.log('Server started...');
+});
